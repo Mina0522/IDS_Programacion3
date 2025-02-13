c472314b2b9de539b705ecea2d80cbc23f03c9c0
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-02-13 14:27:42 -0700

    Actividad 2 - Se desplego un label y un background con colores personalizados.

--- Ventana/src/Vista_Pantalla.java
+++ Ventana/src/Vista_Pantalla.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.*;
 
 public class Vista_Pantalla extends JFrame{
@@ -8,12 +11,31 @@ public class Vista_Pantalla extends JFrame{
 	
 	public Vista_Pantalla () {
 		
+		String color = "#D783D2";
+		
 		this.setSize(500,500);
 		this.setTitle("Ventana");
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		lamina.setBackground(Color.decode(color));
+		this.setBackground(Color.lightGray);
 		this.getContentPane().add(lamina); 
+		this.setVisible(true);
+		
+		JLabel etiqueta = new JLabel();
+		String color2 = "#944491";
+		etiqueta.setText("Bienvenido");
+		etiqueta.setBounds(50,50,50,50);
+		etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+		etiqueta.setForeground(Color.white);
+		etiqueta.setOpaque(true);
+		etiqueta.setBackground(Color.black);
+		etiqueta.setFont(new Font("arial", Font.PLAIN,50));
+		etiqueta.setBackground(Color.decode(color2));
+		
+		lamina.add(etiqueta);
 		this.setVisible(true);
 	}
 
@@ -23,3 +45,8 @@ public static void main(String[] args) {
     
 	}
 }
+
+
+
+
+//{}
