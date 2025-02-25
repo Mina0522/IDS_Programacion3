09c6da1f87fca92bcbc637d07db055d81fce3bfa
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-02-25 09:40:53 -0700

    Unidad 2 - Ejercicio 20 - Item

--- Ventana/src/Vista_Pantalla.java
+++ Ventana/src/Vista_Pantalla.java
@@ -6,22 +6,13 @@ import javax.swing.ImageIcon;
 
 //implements ActionListener
 public class Vista_Pantalla extends JFrame {
-	
-//	private JLabel etiqueta1,etiqueta2;
-//	private JTextField campoTexto;
-//	private JPasswordField campoPass;
-//	private JButton boton;
-//	private JCheckBox caja;
-	
-//	public JPanel lamina = new JPanel ();
+
 	public Vista_Pantalla () {
 		
-		setSize(657,360);
+		setSize(657, 360);
 		setResizable(false);
+		
 		login ();
-//		vistaPricipal();
-//		elementos();
-
 	}
 	
 	private void login () {
@@ -70,7 +61,7 @@ public class Vista_Pantalla extends JFrame {
 		JButton boton = new JButton("Ingresar");
 		boton.setBounds(250, 260, 120, 25);
 		panelLogin.add(boton);
-//		boton.addActionListener(e -> panel1());
+		boton.addActionListener(e -> panel1());
 
 		JCheckBox caja = new JCheckBox("Recordar contraseña.");
 		caja.setBounds(250, 200, 180, 25);
@@ -80,6 +71,86 @@ public class Vista_Pantalla extends JFrame {
 		
 		setVisible(true);
 	}
+	
+	private void panel1 () {
+ 		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		panelLogin.setBounds(1, 1, 657, 360);
+		setSize(657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Reportes");
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Crear cuenta");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("cooper black",Font.PLAIN,30));
+		panelLogin.add(etq);
+		JLabel etq1 = new JLabel(new ImageIcon("fondoetq1.jpg"));
+		etq1.setBounds(1, 1, 657, 360);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("cooper black",Font.PLAIN,15));
+		etq2.setBounds(180, 90, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("cooper black",Font.PLAIN,15));
+		etq3.setBounds(150, 110, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 90, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 120, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+
+		JButton boton = new JButton("Ingresar");
+		boton.setBounds(250, 260, 120, 25);
+		panelLogin.add(boton);
+//		boton.addActionListener(e -> panel1());
+
+		JCheckBox caja = new JCheckBox("Recordar contraseña.");
+		caja.setBounds(250, 200, 180, 25);
+		panelLogin.add(caja);
+		
+		
+		panelLogin.add(etq1);
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();	
+ 	}
 
 	public static void main(String[] args) {
 		
