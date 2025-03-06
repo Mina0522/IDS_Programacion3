3ae7e46034428948b0e23d68844f99770b724df5
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-03-05 19:32:26 -0700

    EJercicio 31 - Diseño de casa con la  librería grapchis.

--- Ventana/src/Graficos.java
+++ Ventana/src/Graficos.java
@@ -31,9 +31,8 @@ public class Graficos extends JFrame{
 private void login () {
 		
 		JPanel panelLogin = new JPanel ();
-		setSize(1000, 750);
+		setSize(700, 650);
 		setResizable(false);
-		panelLogin.setBounds(20, 10, 657, 360);
 		panelLogin.setLayout(null);
 		
 		this.getContentPane().add(panelLogin);
@@ -43,10 +42,82 @@ private void login () {
 	
 public void paint (Graphics g) {
 	
+	String blue = "#4E98D9";
+	String brown = "#765536";
+	String green = "#5F8C1C";
+	String green2 = "#455922";
+	
+	String verdelimon = "#D2D904";
+	String amarillopollo = "#EEF279";
+	String RED = "#D80903";
+	String gg = "#8C8954";
+	
+	
 	super.paint(g);
 	Graphics2D g2 = (Graphics2D) g;
 	
-	g2.clearRect(100, 100, 50, 50);
+	// == Cielo == 
+	
+	g2.setColor(Color.decode(blue));
+	g2.fillRect(0, 0, 700, 650);
+	
+	// == Suelo ==
+	
+	g2.setColor(Color.decode(brown));
+	g2.fillRect(0, 600, getWidth(), 50);
+	
+	g2.setColor(Color.decode(green));
+	g2.fillRect(0, 560, getWidth(), 40);
+	
+	g2.setColor(Color.decode(green2));
+	g2.fillRect(0, 550, getWidth(), 10);
+	
+	// == Casa ==
+	
+	g2.setColor(Color.decode(verdelimon)); // == Cuerpo de la casa
+	g2.fillRect(90, 245, 270, 320);
+	
+    int[] xTecho = {90, 225, 360}; // Techo
+    int[] yTecho = {245, 120, 245};
+    g2.setColor(Color.decode(brown));
+    g2.fillPolygon(xTecho, yTecho, 3);
+    
+	g2.setColor(Color.decode(RED)); // Puerta
+    g2.fillRect(120, 415, 80, 150);
+    g2.setColor(Color.WHITE);
+    g2.fillOval(190, 490, 5, 5);
+	
+    g2.setColor(Color.decode(amarillopollo)); // Ventana
+    g2.fillRect(220, 320, 120, 120);
+    g2.setColor(Color.decode(RED));
+    g2.drawRect(220, 320, 120, 120);
+    g2.drawLine(280, 320, 280, 440);
+    g2.drawLine(220, 380, 340, 380);
+    
+    // === EXTRA
+    
+    g2.setColor(Color.yellow);
+    g2.fillOval(400, 520, 50, 30);
+    g2.fillOval(440, 510, 20, 20); 
+    g2.setColor(Color.ORANGE);
+    g2.fillPolygon(new int[]{460, 470, 460}, new int[]{515, 520, 525}, 3);
+    g2.setColor(Color.BLACK);
+    g2.fillOval(455, 515, 5, 5);
+
+    g2.setColor(Color.white);
+    g2.fillOval(400, 70, 50, 50);
+    g2.fillOval(430, 70, 50, 50);
+    
+    g2.fillOval(510, 70, 50, 50);
+    g2.fillOval(530, 60, 60, 60);
+    g2.fillOval(560, 70, 50, 50);
+    
+    g2.fillOval(20, 60, 60, 60);
+    g2.fillOval(60, 50, 70, 70);
+    g2.fillOval(100, 60, 60, 60);
+    
+    
+	/*g2.clearRect(100, 100, 50, 50);
 	g2.drawRect(80, 80, 400, 200);
 	g2.fillRect(200, 200, 200, 400);
 	g2.setColor(Color.blue);
@@ -79,7 +150,7 @@ public void paint (Graphics g) {
 	int[] xs = {100, 100, 400};
 	int[] ys = {100, 100, 400};
 	
-	g2.drawPolygon(xs, ys, 3);
+	g2.drawPolygon(xs, ys, 3);*/
 
 	
 }
