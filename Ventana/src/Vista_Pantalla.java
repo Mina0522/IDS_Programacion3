11432e24dca089645683cdf47c41c492e50e8a77
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-03-13 08:08:41 -0700

    Unidad 4 - Actividad 7 - Crear conexion entre varias pantallas.

--- Ventana/src/Vista_Pantalla.java
+++ Ventana/src/Vista_Pantalla.java
@@ -27,7 +27,7 @@ public class Vista_Pantalla extends JFrame {
 	private void login () {
 		
 		JPanel panelLogin = new JPanel ();
-		setSize(657, 360);
+		setSize(700, 500);
 		setResizable(false);
 		panelLogin.setBounds(20, 10, 657, 360);
 		panelLogin.setLayout(null);
@@ -41,7 +41,7 @@ public class Vista_Pantalla extends JFrame {
 		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
 		panelLogin.add(etq);
 		
-		//
+
 //		JLabel etq1 = new JLabel(new ImageIcon("fondoetq1.jpg"));
 //		etq1.setBounds(1, 1, 657, 360);
 		
@@ -73,11 +73,60 @@ public class Vista_Pantalla extends JFrame {
 		JCheckBox caja = new JCheckBox("Recordar contraseña.");
 		caja.setBounds(250, 200, 180, 25);
 		panelLogin.add(caja);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Cuenta");
+		JMenu menu2 = new JMenu("Usuarios");
+		JMenu menu3 = new JMenu("Ayuda");
+		
+		JMenuItem item = new JMenuItem("Login");
+		item.addActionListener(e -> loginMenu()); 
+		JMenuItem item1 = new JMenuItem("Registro");
+		item1.addActionListener(e -> registro()); 
+		JMenuItem item2 = new JMenuItem("Recuperar cuenta");
+		item2.addActionListener(e -> recuperarCuenta()); 
+		
+		JMenuItem item3 = new JMenuItem("Alta");
+		item3.addActionListener(e -> alta()); 
+		JMenuItem item4 = new JMenuItem("Baja");
+		item4.addActionListener(e -> baja()); 
+		JMenuItem item5 = new JMenuItem("Consultar");
+		item5.addActionListener(e -> consultar()); 
+		
+		JMenuItem item6= new JMenuItem("¿Como crear un usuario?");
+		item6.addActionListener(e -> crear()); 
+		JMenuItem item7= new JMenuItem("¿Como acceder al sistema?");
+		item7.addActionListener(e -> acceder()); 
+		JMenuItem item8 = new JMenuItem("¿Olvidaste la contraseña?");
+		item8.addActionListener(e -> pass()); 
+		
+		JMenuItem item9 = new JMenuItem("Regresar");
+		item9.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		menu.add(menu3);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		menu1.add(item2);
+		
+		menu2.add(item3);
+		menu2.add(item4);
+		menu2.add(item5);
+		
+		menu3.add(item6);
+		menu3.add(item7);
+		menu3.add(item8);
+		menu3.add(item9);
+		
+		this.setJMenuBar(menu);
 
 		JButton boton = new JButton("Ingresar");
 		boton.setBounds(250, 260, 120, 25);
 		panelLogin.add(boton);
-		boton.addActionListener(e -> panel1());
+		boton.addActionListener(e -> crear());
 		
 		boton.addActionListener(new ActionListener() {
 
@@ -103,12 +152,12 @@ public class Vista_Pantalla extends JFrame {
 		setVisible(true);
 	}
 	
-	private void panel1 () {
+	private void crear () {
  		
  		getContentPane().removeAll();
  		//
 		JPanel panelLogin = new JPanel ();
-		setSize(657, 360);
+		setSize(700, 500);
 		panelLogin.setBounds(20, 10, 657, 360);
 		panelLogin.setLayout(null);
 		this.getContentPane().add(panelLogin);
@@ -205,6 +254,585 @@ public class Vista_Pantalla extends JFrame {
  		repaint();
  		revalidate();
  	}
+	
+	private void loginMenu () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Login Menu");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+//		JLabel etq1 = new JLabel(new ImageIcon("fondoetq1.jpg"));
+//		etq1.setBounds(1, 1, 657, 360);
+//		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 90, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 110, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 90, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 120, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+//		
+//		JCheckBox caja = new JCheckBox("Recordar contraseña.");
+//		caja.setBounds(250, 200, 180, 25);
+//		panelLogin.add(caja);
+//
+//		JButton boton = new JButton("Ingresar");
+//		boton.setBounds(250, 260, 120, 25);
+//		panelLogin.add(boton);
+//		boton.addActionListener(e -> panel1());
+//
+//		JButton botonSalir = new JButton("Regresar");
+//		botonSalir.setBounds(50, 260, 120, 25);
+//		panelLogin.add(botonSalir);
+//		botonSalir.addActionListener(e -> login());
+//
+//	
+//		boton.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println("hola >:I");
+//				
+//				if(text.getText().equals("Romi") || pass.getPassword().equals("aguacate")) {
+//					text.setBorder(BorderFactory.createLineBorder(Color.green));
+//					pass.setBorder(BorderFactory.createLineBorder(Color.green));
+//					System.out.println("Bienvenido!!");
+//				}else {
+//					text.setBorder(BorderFactory.createLineBorder(Color.red));
+//					pass.setBorder(BorderFactory.createLineBorder(Color.red));
+//				}
+//			}});
+//		
+//		panelLogin.add(etq1);
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void registro () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Registro menu");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void recuperarCuenta () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Recuperar Cuenta");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void alta () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Alta de datos");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void baja () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Baja de datos");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void consultar () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Consulta de datos");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+
+	private void acceder () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Acceder a mi cuenta");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
+	
+	private void pass () {
+		
+ 		getContentPane().removeAll();
+ 		//
+		JPanel panelLogin = new JPanel ();
+		setSize(700, 500);
+		panelLogin.setBounds(20, 10, 657, 360);
+		panelLogin.setLayout(null);
+		this.getContentPane().add(panelLogin);
+		
+		JMenuBar menu = new JMenuBar();
+		
+		JMenu menu1 = new JMenu("Ayuda");
+		JMenu menu2 = new JMenu("Archivos");
+		
+		JMenuItem item = new JMenuItem("Atencion al cliente");
+		JMenuItem item1 = new JMenuItem("Configuracion");
+		JMenuItem item2 = new JMenuItem("Clientes");
+		JMenuItem item3 = new JMenuItem("Regresar");
+		item3.addActionListener(e -> login());
+		
+		menu.add(menu1);
+		menu.add(menu2);
+		
+		menu1.add(item);
+		menu1.add(item1);
+		
+		menu2.add(item2);
+		menu2.add(item3);
+		this.setJMenuBar(menu);
+		
+		JLabel etq = new JLabel("Olvide mi contraseña");
+		etq.setBounds(150, 5, 300, 80);
+		etq.setHorizontalAlignment(SwingConstants.CENTER);
+		etq.setForeground(Color.black);
+		etq.setFont(new Font("Century Gothic",Font.PLAIN,30));
+		panelLogin.add(etq);
+		
+		JLabel etq2 = new JLabel("Usuario:");
+		panelLogin.setLayout(null);
+		etq2.setForeground(Color.black);
+		etq2.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq2.setBounds(180, 100, 90, 25);
+		panelLogin.add(etq2);
+		
+		JLabel etq3 = new JLabel("Contraseña:");
+		panelLogin.setLayout(null);
+		etq3.setForeground(Color.black);
+		etq3.setFont(new Font("Century Gothic",Font.PLAIN,15));
+		etq3.setBounds(150, 130, 100, 30);
+		panelLogin.add(etq3);
+		
+		//
+		JTextField text = new JTextField();
+		text.setBounds(320, 100, 180, 25);
+		panelLogin.add(text);
+
+		JPasswordField pass = new JPasswordField();
+		pass.setBounds(320, 130, 180, 25);
+		pass.setEchoChar('*');
+		panelLogin.add(pass);
+		
+		setVisible(true);
+ 		setContentPane(panelLogin); 
+ 		repaint();
+ 		revalidate();
+	}
 	
 	public static void main(String[] args) {
 		
