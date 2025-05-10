c6c2b8a2f4958e983587f1b558a2c9c43d64e4c6
Mina0522 <rominabarriosr.osalesr@gmail.com>
2025-05-10 14:43:27 -0700

    commit de prueba

--- Proyect_ATHLONGYM/src/View/Login.java
+++ Proyect_ATHLONGYM/src/View/Login.java
@@ -1,5 +1,11 @@
 package View;
 
-public class Login {
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame {
+	
 }
